d049eda5718ff3bdeead101ae0a7b5cd53064e8e
Kevin Backup <kvchang@umich.edu>
2024-05-06 15:20:45 -0400

    up-to-date

--- app-crm/src/components/calendar/upcoming-events/queries.ts
+++ app-crm/src/components/calendar/upcoming-events/queries.ts
@@ -1,6 +1,10 @@
 import gql from "graphql-tag";
 
-export const CALENDAR_UPCOMING_EVENTS_QUERY = gql`
+//This GraphQL query retrieves upcoming events based on specific filters, sorting, and paging criteria. 
+//It's structured to work with Apollo Client, a popular GraphQL client in JavaScript applications.
+
+export const CALENDAR_UPCOMING_EVENTS_QUERY = gql
+`
     query UpcomingEvents(
         $filter: EventFilter!
         $sorting: [EventSort!]
